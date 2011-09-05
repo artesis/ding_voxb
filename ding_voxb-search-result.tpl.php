3ebc0b9b10ca9556bbbb1df425d6ce4784e4661a
Dragan <di@inlead.dk>
2011-09-05 15:56:05 +0300

    case 11967 - Removed dirty hooks. Added template for search result.

---
+++ ding_voxb-search-result.tpl.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * @file
+ *
+ */
+?>
+
+<div class="voxb-details">
+  <div class="voxb-rating">
+
+    <p class="left"><?php print t('Rating'); ?>:</p>
+    <div style="margin-left: 10px;" class="rating-star left inactive"></div>
+    <div class="rating-star left inactive"></div>
+    <div class="rating-star left inactive"></div>
+    <div class="rating-star left inactive"></div>
+    <div class="rating-star left inactive"></div>
+    <p class="rating-count left">(<span>0</span>)</p>
+  </div>
+  <div class="voxb-reviews left" style="display: none;">
+    <p class="review-count"><?php print t('Anmeldelser'); ?> (<span class="count">0</span>)</p>
+  </div>
+  <div class="clear"></div>
+</div>

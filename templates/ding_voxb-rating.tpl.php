4015a419157a4f10253a493cd2c3bd5489d91aab
Giannis Chionidis <gc@inlead.dk>
2013-05-31 10:14:11 +0200

    case 2851 - Changed title attr, in href, to represent number of stars.

--- templates/ding_voxb-rating.tpl.php
+++ templates/ding_voxb-rating.tpl.php
@@ -8,7 +8,7 @@ if (!empty($object->isbn)):
 <div class="voxb-details isbn-<?php echo $object->isbn[0]; ?>">
   <div class="voxb-rating <?php echo $rating_block_class; ?>">
     <?php for ($i = 1; $i <= 5; $i++): ?>
-    <a href="/voxb/ajax/rating/<?php echo $object->isbn[0]; ?>/<?php echo $i; ?>" class="rating left <?php echo $star_class; ?>" title="<?php echo $object->title; ?>"></a>
+    <a href="/voxb/ajax/rating/<?php echo $object->isbn[0]; ?>/<?php echo $i; ?>" class="rating left <?php echo $star_class; ?>" title="<?php echo 'Rate with ' . $i . ' star' . ($i > 1 ? 's' : ''); ?>"></a>
     <?php endfor; ?>
     <p class="rating-count left"><span></span></p>
   </div>

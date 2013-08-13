d053693c9f741ee3e7d93b9858b5a48fbbcfb052
George Mihailov <gm@inlead.dk>
2013-08-13 16:31:00 +0300

    case 3025 - Different rating links for enabled and disabled rating.

--- templates/ding_voxb-rating.tpl.php
+++ templates/ding_voxb-rating.tpl.php
@@ -8,7 +8,7 @@ if (!empty($object->isbn)):
 <div class="voxb-details isbn-<?php echo $object->isbn[0]; ?>">
   <div class="voxb-rating <?php echo $rating_block_class; ?>">
     <?php for ($i = 1; $i <= 5; $i++): ?>
-        <?php if (!$display_only) : ?>
+        <?php if ($rate_enabled) : ?>
             <a href="/voxb/nojs/rating/<?php echo $object->isbn[0]; ?>/<?php echo $i; ?>" class="rating left <?php echo $star_class; ?>" title="<?php echo t('Rate with ' . $i . ' star' . ($i > 1 ? 's' : '')); ?>"></a>
         <?php else : ?>
             <a href="javascript:void()" class="display-only rating left <?php echo $star_class; ?>"></a>

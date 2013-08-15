cd2c98bd1d189d0f954dd79c20c55eeca58dc876
George Mihailov <gm@inlead.dk>
2013-08-15 16:36:02 +0300

    case 3025 - Change the way of preventing of rate click.

--- templates/ding_voxb-rating.tpl.php
+++ templates/ding_voxb-rating.tpl.php
@@ -11,7 +11,7 @@ if (!empty($object->isbn)):
         <?php if ($rate_enabled) : ?>
             <a href="/voxb/nojs/rating/<?php echo $object->isbn[0]; ?>/<?php echo $i; ?>" class="rating left <?php echo $star_class; ?>" title="<?php echo t('Rate with ' . $i . ' star' . ($i > 1 ? 's' : '')); ?>"></a>
         <?php else : ?>
-            <a href="javascript:void()" class="display-only rating left <?php echo $star_class; ?>"></a>
+            <a href="#" target="_blank" onclick="return false" class="display-only rating left <?php echo $star_class; ?>"></a>
         <?php endif ?>
     <?php endfor; ?>
     <p class="rating-count left" title="<?php echo t('Number of ratings') ?>"><span></span></p>
